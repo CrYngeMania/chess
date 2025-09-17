62bd043fe24bc88ace189d1cf2148bef5c9a08b8
CrYngeMania <jainmed2005@gmail.com>
2025-09-16 22:28:02 -0600

    Implement Phase 0 (board, moves, etc) (my first commit apparently didn't go through so I also have moves for single steps implemented)

--- shared/src/main/java/chess/ChessPiece.java
+++ shared/src/main/java/chess/ChessPiece.java
@@ -1,8 +1,6 @@
 package chess;
 
-import java.util.Collection;
-
-import java.util.HashSet;
+import java.util.*;
 
 /**
  * Represents a single chess piece
@@ -12,7 +10,13 @@ import java.util.HashSet;
  */
 public class ChessPiece {
 
+    private final ChessGame.TeamColor pieceColor;
+    private final PieceType type;
+
     public ChessPiece(ChessGame.TeamColor pieceColor, ChessPiece.PieceType type) {
+        this.pieceColor = pieceColor;
+        this.type = type;
+        boolean hasMoved = false;
     }
 
     /**
@@ -31,14 +35,14 @@ public class ChessPiece {
      * @return Which team this chess piece belongs to
      */
     public ChessGame.TeamColor getTeamColor() {
-        throw new RuntimeException("Not implemented");
+        return pieceColor;
     }
 
     /**
      * @return which type of chess piece this piece is
      */
     public PieceType getPieceType() {
-        throw new RuntimeException("Not implemented");
+        return type;
     }
 
     /**
@@ -48,7 +52,130 @@ public class ChessPiece {
      *
      * @return Collection of valid moves
      */
-    public Collection<ChessMove> pieceMoves(ChessBoard board, ChessPosition myPosition) {
-        return new HashSet<ChessMove>();
+
+    private static class Rule{
+        boolean moveOnce;
+        List<ChessPosition> directions;
+        Rule(boolean moveOnce, List<ChessPosition> directions){
+            this.directions = directions;
+            this.moveOnce = moveOnce;
+
+        }
+        static Collection<ChessMove> movesFromPiece(ChessPiece piece, ChessPosition myPosition){
+            Rule typerule = RuleLibrary.getRule(piece);
+            HashSet<ChessMove> moves = new HashSet<>();
+            for (ChessPosition direction : typerule.directions){
+
+                ChessPosition nextPos = ChessPosition.add(myPosition, direction);
+                int newRow = nextPos.getRow();
+                int newCol = nextPos.getColumn();
+                if (0 < newRow && newRow < 8){
+                    if (0 < newCol && newCol < 8){
+                        ChessMove newMove = new ChessMove(myPosition, nextPos, null);
+                        moves.add(newMove);
+                    }
+                }
+
+                }
+            return moves;
+            }
+
+
+
+
+
+    };
+
+    private static class RuleLibrary {
+
+        static Rule getRule(ChessPiece piece) {
+            PieceType type = piece.getPieceType();
+            return rules(type);
+        }
+
+        private static Rule rules(PieceType type) {
+
+            return switch (type) {
+                case BISHOP -> new Rule(false, Arrays.asList(
+                        new ChessPosition(1, 1),
+                        new ChessPosition(1, -1),
+                        new ChessPosition(-1, -1),
+                        new ChessPosition(-1, 1)));
+                case ROOK -> new Rule(false, Arrays.asList(
+                        new ChessPosition(1, 0),
+                        new ChessPosition(0, -1),
+                        new ChessPosition(-1, 0),
+                        new ChessPosition(0, 1)));
+                case QUEEN -> new Rule(false, Arrays.asList(
+                        new ChessPosition(1, 1),
+                        new ChessPosition(1, -1),
+                        new ChessPosition(-1, -1),
+                        new ChessPosition(-1, 1),
+                        new ChessPosition(1, 0),
+                        new ChessPosition(0, -1),
+                        new ChessPosition(-1, 0),
+                        new ChessPosition(0, 1)));
+                case KING -> new Rule(true, Arrays.asList(
+                        new ChessPosition(1, 1),
+                        new ChessPosition(1, -1),
+                        new ChessPosition(-1, -1),
+                        new ChessPosition(-1, 1),
+                        new ChessPosition(1, 0),
+                        new ChessPosition(0, -1),
+                        new ChessPosition(-1, 0),
+                        new ChessPosition(0, 1)));
+                case KNIGHT -> new Rule(true, Arrays.asList(
+                        new ChessPosition(1, -2),
+                        new ChessPosition(2, -1),
+                        new ChessPosition(2, 1),
+                        new ChessPosition(1, 2),
+                        new ChessPosition(-1, 2),
+                        new ChessPosition(-2, 1),
+                        new ChessPosition(-2, -1),
+                        new ChessPosition(-1, -2)));
+                case PAWN -> new Rule(true, Arrays.asList(
+                        new ChessPosition(0, 1),
+                        new ChessPosition(0, 2)));
+                default -> null;
+            };
+        }
+        }
+
+
+        public Collection<ChessMove> pieceMoves(ChessBoard board, ChessPosition myPosition) {
+            Collection<ChessMove> moves;
+            ChessPiece piece = board.getPiece(myPosition);
+            moves = Rule.movesFromPiece(piece, myPosition);
+
+            /**
+             * Implement Bishop and Rook first
+             * Queen can use both Bishop and Rook (thats pretty much it for Queen)
+             * Knight
+             * King
+             * Pawn (pawn is hardest)
+             * implement in ChessPosition?
+             * i am losing my mind here I feel like this shouldn't be so hard for me :(
+             */
+
+
+            return moves;
+        }
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        ChessPiece that = (ChessPiece) o;
+        return pieceColor == that.pieceColor && type == that.type;
     }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(pieceColor, type);
+    }
+
+
 }
+
