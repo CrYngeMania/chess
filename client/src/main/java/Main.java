44ae84ecce79a9f95e4fee0967d26a05b500d1b4
CrYngeMania <jainmed2005@gmail.com>
2025-11-15 00:31:46 -0700

    annnnddddddd lets try this one

--- client/src/main/java/Main.java
+++ client/src/main/java/Main.java
@@ -2,12 +2,12 @@ import chess.*;
 import client.PreLoginClient;
 import dataaccess.DataAccessException;
 import server.Server;
+import client.PreLoginClient;
 
 public class Main {
 
-    }
-
     public static void main(String[] args) {
-    String serverUrl = "http://localhost:8080";
-    new PreLoginClient(serverUrl).run();
+        String serverUrl = "http://localhost:8080";
+        new PreLoginClient(serverUrl).run();
     }
+}
