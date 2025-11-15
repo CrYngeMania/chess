dd5a0d88b4a269a0257f51cdbdf72bd3c7350f73
CrYngeMania <jainmed2005@gmail.com>
2025-11-15 00:09:43 -0700

    still trying to see if i can get it to compile

--- client/src/main/java/Main.java
+++ client/src/main/java/Main.java
@@ -8,15 +8,6 @@ public class Main {
     }
 
     public static void main(String[] args) {
-
-        Server server = new Server();
-        var port = server.run(0);
-        System.out.println("Started test HTTP server on " + port);
-
-        String serverUrl = "http://localhost:8080";
-        if (args.length == 1) {
-            serverUrl = args[0];
-        }
-
-        new PreLoginClient(serverUrl).run();
+    String serverUrl = "http://localhost:8080";
+    new PreLoginClient(serverUrl).run();
     }
